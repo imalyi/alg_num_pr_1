0e15df8e2f126ce57ae25c94717fb0ffa7e2a4ba
SheeyeDev <102896476+SheeyeDev@users.noreply.github.com>
2023-03-31 13:45:17 +0200

    Sprawdzanie czy plik istnieje

--- lagrange/getData.py
+++ lagrange/getData.py
@@ -1,10 +1,14 @@
 class GetData():
-	def getData():
-	    data = []
-	    f = open("./data.txt")
-	    lines = f.readlines()
-	    for x in range(len(lines)):
-		    arg = lines[x].split()
-		    data.append([int(arg[0]),int(arg[1])])
-	    return data
+	def getData(self):
+		data = []
+		try:
+			f = open("./data.txt")
+		except FileNotFoundError:
+			print("PLik nie istnieje!")
+			exit()
+		lines = f.readlines()
+		for x in range(len(lines)):
+			arg = lines[x].split()
+			data.append([int(arg[0]),int(arg[1])])
+		return data
 
--- lagrange/main.py
+++ lagrange/main.py
@@ -1,31 +1,15 @@
-# TODO: print sqrt
-# TODO: add data input
-# TODO: plot graphs
-# TODO: fix latex report
-# TODO: add common formula
+# print sqrt
+# add data input
 
 from polynomials import Polynomials
-
+from getData import GetData
 
 def main():
-    data_raw = [
-            [0, 0],
-            [1, 1],
-            [4, 2],
-            [9, 3],
-            [16, 4],
-            [25, 5],
-            [36, 6],
-            [49, 7],
-            [64, 8],
-            [81, 9]
-        ]
+    data_raw = GetData.getData(GetData)
     p = Polynomials(data_raw)
     best = p.find_best()
-    for b in best:
-        print(b)
-#     best.report.save()
-
+    print(best)
+    best.report.save()
 
 if __name__ == "__main__":
     main()
