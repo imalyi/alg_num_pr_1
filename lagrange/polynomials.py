c0e06a21dfac76cdc13b5804f218011c6f1ebd5d
Anna Ćwiklińska <acwiklinska2002@o2.pl>
2023-04-14 14:46:27 +0200

    usuniecie komentarzy

--- lagrange/polynomials.py
+++ lagrange/polynomials.py
@@ -4,26 +4,11 @@ from data import DataSets
 
 
 class Polynomials:
-    """
-    A class that represents a collection of polynomial objects that can be used to find the best polynomial
-    approximation for a given set of data.
-    """
 
     def __init__(self, datasets: list):
-        """
-        Initializes an object of the Polynomials class with a single parameter - datasets.
-
-        :param datasets: A DataSets object containing a list of data points.
-        """
         self.datasets = DataSets(datasets)
 
     def find_best(self):
-        """
-        Computes a set of polynomial objects for each dataset in the Polynomials object and returns the best
-        polynomial approximation for the dataset.
-
-        :return: A polynomial object that best approximates the dataset.
-        """
         polynomials = []
         for data in self.datasets:
             p = Polynome(data)
